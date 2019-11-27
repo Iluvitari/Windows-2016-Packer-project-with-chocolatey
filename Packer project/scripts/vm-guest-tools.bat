d548498512a0d90bfa03f9b9e25b1299c023d222
Iluvitari <48405834+Iluvitari@users.noreply.github.com>
2019-11-27 13:55:51 +0000

    v2.7 Fixed json and .bat files
    Added microsoft updates to autounattend file.
    
    Changed shell to windows-shell in provisioner
    
    removed copy file of vm guest tools
    
    The packer file now runs and functions all the way to the end with no errors.
    
    Next step is to clean up unused scripts and create a script to hook the server up to AD and have it update with wsus instead of windows updates.

--- Packer project/scripts/vm-guest-tools.bat
+++ Packer project/scripts/vm-guest-tools.bat
@@ -36,12 +36,13 @@ goto :done
 
 :virtualbox
 
-move /Y C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
+move /Y C:\Users\Project\VBoxGuestAdditions.iso C:\Windows\Temp
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
 cmd /c for %%i in (C:\Windows\Temp\virtualbox\cert\vbox*.cer) do C:\Windows\Temp\virtualbox\cert\VBoxCertUtil add-trusted-publisher %%i --root %%i
 cmd /c C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
 goto :done
 
+:done
 msiexec /qb /x C:\Windows\Temp\7z920-x64.msi
 
 echo "Done!"
